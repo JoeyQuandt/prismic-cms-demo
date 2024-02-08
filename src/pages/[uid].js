4be2cc7305be5a9bf5850965925abeeeba3a5e82
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-02-08 10:32:59 +0100

    feat: demo setup

---
+++ src/pages/[uid].js
@@ -0,0 +1,42 @@
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { asLink } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
+
+import { components } from "@/slices";
+import { createClient } from "@/prismicio";
+
+export default function Page({ page }) {
+  return (
+    <>
+      <SliceZone slices={page.data.slices} components={components} />
+    </>
+  );
+}
+
+export async function getStaticProps({ params, previewData }) {
+  // The `previewData` parameter allows your app to preview
+  // drafts from the Page Builder.
+  const client = createClient({ previewData });
+
+  const page = await client.getByUID(
+    "page",
+    params.uid ? params.uid[0] : "home"
+  );
+  return {
+    props: { page },
+  };
+}
+
+export async function getStaticPaths() {
+  const client = createClient();
+
+  const pagesPaths = await client.getAllByType("page");
+
+  let pages = pagesPaths.map((page) => {
+    return asLink(page);
+  });
+  return {
+    paths: [...pages, "/"],
+    fallback: false,
+  };
+}
